047755c229e20687f6f42cf462c4df2204ced2b9
Ricardo Coutinho <jrsscoutinho1@gmail.com>
2020-05-12 17:07:43 -0400

    Utilizando Unform em 04:00

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import { shade } from 'polished';
+
+export const Container = styled.button`
+  background: #FF9000;
+  border-radius: 10px;
+  border: 0;
+  padding:  0 16px;
+  width: 100%;
+  height: 56px;
+  font-weight: 500;
+  margin-top: 16px;
+  transition: background-color 0.2s;
+
+  &:hover{
+    background: ${shade(0.2, '#FF9000')}
+  }
+`;
---
+++ src/pages/signup/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { Form } from '@unform/web';
+
+import logo from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Background, Content } from './styles';
+
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+
+  return (
+    <Container>
+      <Background />
+
+      <Content>
+        <img src={logo} alt="GoBarber" />
+        <Form onSubmit={handleSubmit} >
+          <h1>Faça seu Cadastro</h1>
+          <Input icon={FiUser} name="name" placeholder="Nome" />
+          <Input icon={FiMail} name="email" placeholder="E-mail" />
+          <Input icon={FiLock} name="password" type="password" placeholder="Senha" />
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+        <a href="login">
+          <FiArrowLeft />
+        Voltar para logon
+      </a>
+      </Content>
+    </Container>
+  )
+};
+
+export default SignUp;
